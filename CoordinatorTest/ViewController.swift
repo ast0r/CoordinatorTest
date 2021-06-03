f349f1790c75f3fe5e9f078bc05f645180153c08
Ivanov Pavel <ast0r89@gmail.com>
2021-06-03 14:15:33 +0300

    Initial Commit

---
+++ CoordinatorTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorTest
+//
+//  Created by Ivanov Pavlo on 03.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
